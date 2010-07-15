2bf7ec0bd5a7d6f90705eb4de0f0e862cb4d1ef9
Jakob Ovrum <jakobovrum@gmail.com>
2010-07-15 18:01:40 +0900

    Oops, forgot object.cpp

---
+++ src/object.cpp
@@ -0,0 +1,22 @@
+#include <luacpp/object.hpp>
+#include <luacpp/lua.hpp>
+
+namespace lua
+{
+	bool object::operator==(const reference& r)
+	{
+		push();
+		r.push();
+		bool b = lua_equal(state(), -1, -2);
+		lua_pop(state(), 2);
+		return b;
+	}
+
+	bool object::operator==(const nil_t& n)
+	{
+		push();
+		bool b = lua_isnil(state(), -1);
+		lua_pop(state(), 1);
+		return b;
+	}
+}
