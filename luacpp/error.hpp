59380eea28984516062b885d7ea2a009fec43098
Jakob Ovrum <jakobovrum@gmail.com>
2010-07-16 18:24:21 +0900

    Updated documentation and Doxyfile

--- luacpp/error.hpp
+++ luacpp/error.hpp
@@ -7,7 +7,12 @@ namespace lua
 {
 	///Lua error exception
 	/**
-		Thrown when an unhandled Lua error is raised (Lua panic).
+		Thrown when an unhandled Lua error is raised.
+		Keep in mind that the actual C++ exception is only thrown if Lua
+		doesn't know what to do with one of its errors (Lua panic). This
+		means that all cases of lua::error being thrown (as referenced in documentation)
+		can, and will, be caught by protected Lua calls before being propagated as
+		this exception.
 	*/
 	class error : public std::runtime_error
 	{
